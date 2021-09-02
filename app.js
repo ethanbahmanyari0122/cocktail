103ad97051bfde0e8240593e97e97e13065db045
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-09-02 13:52:45 +0200

    server and all dependencies are up and running

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 const port = process.env.PORT || 3000;
-const ejs = require('ejs');
+//const ejs = require('ejs');
 
 app.get('/',(req, res) => {
-    res.send('what up cocktail people');
+    res.sendFile(__dirname+'/Data.html');
 })
 
-
-
+app.get('/about',(req, res) => {
+    res.sendFile(__dirname+'/Aboutus.html');
+})
 
 
 
