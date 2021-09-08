e79ba3543cc0f77afd20a5a5b6abea77bcf62d8e
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-09-08 11:02:58 +0200

    some basics to the frontend has been added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const https = require('https');
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
