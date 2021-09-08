97f04422480ec0ff6e5940ce02e704d73ac04e13
Rawezh <70198090+Rawezhham176@users.noreply.github.com>
2021-09-08 13:15:08 +0200

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,8 +1,38 @@
-const request = require("supertest");
-const app = require("../app");
+var request = require('supertest');
+var app = require('../app');
 
-describe("GET /", () => {
-  it("respond with ", (done) => {
-    request(app).get("/").expect("home", done);
-  })
-});
+
+request(app)
+       .get('/')
+       .expect(200)
+       .end((err, res) => {
+         if(err) throw err;
+       })
+
+describe('GET home', function() {
+        it('responds with ejs', function(done) {
+          request(app)
+            .get('/')
+            .expect(200, done);
+        });
+      });       
+
+
+describe('GET About', function() {
+        it('responds with ejs', function(done) {
+          request(app)
+            .get('/about')
+            .expect('Content-Type', /html/)
+            .expect(200)
+            .end(function(err, res) {
+              if (err) return done(err);
+              return done();
+            });
+        });
+      });      
+
+describe('hi', async () => {
+  await request(app).get('/contact')
+                    .expect(200)
+})
+
