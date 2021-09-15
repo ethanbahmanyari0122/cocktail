983c1ab0914fda53726c2815addf1eda7f3ed98a
Mika Schulz <mika.schulz@otto.de>
2021-09-15 08:52:58 +0200

    Refactor app.js

--- app.js
+++ app.js
@@ -1,57 +1,74 @@
+// Include Node.js modules
 const express = require('express');
 const bodyParser = require('body-parser');
 const superagent = require('superagent');
 const ejs = require('ejs');
 const lodash = require('lodash');
 
+// Create an object of the express module
 const app = express();
+
+// Define future variables
 let cocktails;
 let notCocktails;
 let eachRecipe;
-const alcoholicURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?"
-const nonAlcoholicURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic"
-const recipeURL = "www.thecocktaildb.com/api/json/v1/1/lookup.php?"
+const filterUrl = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?"
+const recipeURL = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?"
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-
+// Set variable 'port' to the port provided by the environment
+// Set variable 'port' to 3000 if no port is being provided
 const port = process.env.PORT || 3000;
 
-superagent.get(alcoholicURL)
+// Get HTTP-response from "filterUrl" with the query parameter "c=Cocktail" and save it to "cocktails"
+superagent.get(filterUrl)
     .query({c:'Cocktail'})
     .end((err, response) => {
         if (err) { return console.log(err); }
         cocktails = response.body.drinks;
 
     });
-superagent.get(nonAlcoholicURL)
+
+// Get HTTP-response from "filterUrl" with the query parameter "a=Non_Alcoholic" and save it to "notCocktails"
+superagent.get(filterUrl)
     .query({a:'Non_Alcoholic'})
     .end((err, response) => {
         if (err) { return console.log(err); }
         notCocktails = response.body.drinks;
     });
 
+// set the view engine of the express application to ejs
 app.set('view engine', 'ejs');
 
+// Render home.ejs when user enters website
 app.get('/',(req, res) => {
     res.render("home");
 })
 
+// Render about.ejs when user enters "/about"
 app.get('/about',(req, res) => {
     res.render("about");
 })
 
+// Render alcoholic.ejs when user enters "/alcoholic"
 app.get('/alcoholic',(req, res) => {
     res.render("alcoholic", {cocktails: cocktails});
 })
 
+// Render non_alcoholic.ejs when user enters "/non_alcoholic"
 app.get('/non_alcoholic',(req, res) => {
     res.render("non_alcoholic", {notCocktails: notCocktails});
 })
 
+// Lookup users recipe in API
+// User sends the recipe name in ":recipe"
 app.get('/recipes/:recipe', (req, res) => {
+    // Loop through all recipes in "cocktails"
     cocktails.forEach(recipe => {
+        // Check if current recipes name ("strDrink") is equal to users ":recipe"
         if (lodash.lowerCase(req.params.recipe) === lodash.lowerCase(recipe.strDrink)){
+            // Get HTTP-response from "recipeURL" with the query parameter "i=idDrink"
             superagent.get(recipeURL)
                 .query({i: recipe.idDrink})
                 .end((err, response) => {
@@ -60,28 +77,25 @@ app.get('/recipes/:recipe', (req, res) => {
                     res.render('recipe' ,{recipe, eachRecipe:eachRecipe});
         })
 
-
     }})
     notCocktails.forEach(recipe => {
         if (lodash.lowerCase(req.params.recipe) === lodash.lowerCase(recipe.strDrink)){
             superagent.get(recipeURL)
                 .query({i:recipe.idDrink})
                 .end((err, response) => {
-                    if (err) {
-                        return console.log(err);
-                    }
+                    if (err) { return console.log(err); }
+                    // Save the drinks api information to "eachRecipe"
                     eachRecipe = response.body.drinks;
+                    // Render recipe.ejs with option parameters "recipe" and "eachRecipe"
                     res.render('recipe', {recipe, eachRecipe: eachRecipe});
                 })
-
         }
     })
 })
 
-
-
+// export this module to make it accessible from other classes
 module.exports = app; 
 
-
+// Start listening the server on "port"
 app.listen(port,()=>
-    console.log("Server started on port 3000"));
+    console.log("Server started on port " + port));
